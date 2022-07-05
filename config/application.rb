6648336e7b36527e2453e23f4cbb5bb9d0488eda
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-07-06 03:37:20 +0900

    taskとmodelのspecを作成

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module GoCompt
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -16,11 +16,13 @@ module GoCompt
     config.generators do |g|
       g.assets false
       g.helper false
+      g.test_framework :rspec,
+                        model_specs: true,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: false,
+                        request_specs: false
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
