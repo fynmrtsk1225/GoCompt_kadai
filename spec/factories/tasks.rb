4a31db368139806224e60d49deacaa7bb00b52f3
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-07-07 13:53:23 +0900

    新しいタスクが一番上に表示されるテストを作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     priority { 'test_priority' }
     status { 'test_status' }
   end
+
+  factory :task2 do
+    title { 'test_title2' }
+    content { 'test_content2' }
+    deadline { 'test_deadline2' }
+    priority { 'test_priority2' }
+    status { 'test_status2' }
+  end
 end
