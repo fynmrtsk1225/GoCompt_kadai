32f240ceae5cb12e1daaf3beede2f784867d27a1
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-07-22 23:43:09 +0900

    seeds.rbの内容を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create([
-  {id: 0, name: 'Daiki', email: 'daiki@exam.com', password: 'daikidaiki', password_confirmation: 'daikidaiki', admin: true},
-  {id: 1, name: 'Natsuko', email: 'natsuko@exam.com', password: 'natsukonatsuko', password_confirmation: 'natsukonatsuko'}
+  {id: 0, name: 'Aono', email: 'aono@exam.com', password: 'aonoaono', password_confirmation: 'aonoaono', admin: true},
+  {id: 1, name: 'Endo', email: 'endo@exam.com', password: 'endoendo', password_confirmation: 'endoendo'},
+  {id: 2, name: 'Kadowaki', email: 'kadowaki@exam.com', password: 'kadowakikadowaki', password_confirmation: 'kadowakikadowaki'},
+  {id: 3, name: 'Kaneko', email: 'kaneko@exam.com', password: 'kanekokaneko', password_confirmation: 'kanekokaneko'},
+  {id: 4, name: 'Saito', email: 'saito@exam.com', password: 'saitosaito', password_confirmation: 'saitosaito'},
+  {id: 5, name: 'Sato', email: 'sato@exam.com', password: 'satosato', password_confirmation: 'satosato'},
+  {id: 6, name: 'Nakamura', email: 'nakamura@exam.com', password: 'nakamuranakamura', password_confirmation: 'nakamuranakamura'},
+  {id: 7, name: 'Suzuki', email: 'suzuki@exam.com', password: 'suzukisuzuki', password_confirmation: 'suzukisuzuki'},
+  {id: 8, name: 'Maruoka', email: 'maruoka@exam.com', password: 'maruokamaruoka', password_confirmation: 'maruokamaruoka'},
+  {id: 9, name: 'Nishikawa', email: 'nishikawa@exam.com', password: 'nishikawanishikawa', password_confirmation: 'nishikawanishikawa'},
+  {id: 10, name: 'Yoshida', email: 'yoshida@exam.com', password: 'yoshidayoshida', password_confirmation: 'yoshidayoshida'},
+  {id: 11, name: 'Yamada', email: 'yamada@exam.com', password: 'yamadayamada', password_confirmation: 'yamadayamada'},
+  {id: 12, name: 'Moritsuka', email: 'moritsuka@exam.com', password: 'moritsukamoritsuka', password_confirmation: 'moritsukamoritsuka'}
   ])
 
-  5.times do |i|
-    Label.create!(name: "sample#{i + 1}")
+  15.times do |i|
+    Label.create!(name: "label#{i + 1}")
+  end
+
+  15.times do |i|
+    Task.create!(user_id: '0', title: "sample#{i + 1}", content: "sample#{i + 1}", deadline: '2058-03-07', priority: '高', status: '未着手', label_ids: "#{i + 1}" )
   end
